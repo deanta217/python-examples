c3d349c7ae771f1c28be807335d8e98a2a4d2e0a
DeanTA217 <119976172+deanta217@users.noreply.github.com>
2024-04-09 18:27:13 -0400

    Added binary2decimal.py
    This program will convert your binary to decimal. Includes check about being a number.

---
+++ InteractiveScripts/binary2decimal.py
@@ -0,0 +1,20 @@
+import sys
+print("Welcome!\nThis script will convert binary to decimal!")
+while True:
+    binary = input("Enter your binary string.\n")
+    if not binary.isdigit():
+        print("Enter a number.\n")
+        continue
+    decimal = int(binary, 2)
+    #converts input to decimal. 2 represents the base.
+    print("Your decimal is {}".format(decimal))
+    while True:
+        convertmore = input("Would you like to convert another binary string? (Y/N)\n")
+        if convertmore.upper() == "Y":
+            break
+        elif convertmore.upper() == "N":
+           print("Goodbye.")
+           quit()
+           #ends program
+        else:
+           print("Enter Y or N.\n")
