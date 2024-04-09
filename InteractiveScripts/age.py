ad6d3fafdbf903fc28f74125c6faa202bd0fa011
DeanTA217 <119976172+deanta217@users.noreply.github.com>
2024-04-09 18:02:39 -0400

    added negative number check
    also finished age.py

--- InteractiveScripts/age.py
+++ InteractiveScripts/age.py
@@ -21,7 +21,10 @@ while True:
         #since the bracket is the first bracket, it is replaced by the first thing in the .format() part.
         #this is necessary so there isnt a space inbetween the birthyear and the exclamation mark.
     else:
-        break
+        if int(birthyear) < 0:
+            print("Please enter a positive number as your birth year.")
+        else:
+            break
     #this breaks itself out of the loop.
 age = int(year)-int(birthyear)
 #this subtracts the year from the birthyear.
