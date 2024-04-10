2de56c13e13f88c8fba6053e32f217d260bba1c7
DeanTA217 <119976172+deanta217@users.noreply.github.com>
2024-04-10 16:57:34 -0400

    Fixed repeating when converting more strings
    I changed how it works, because I previously changed it when creating the launcher.

--- InteractiveScripts/binary2decimal.py
+++ InteractiveScripts/binary2decimal.py
@@ -1,4 +1,5 @@
-import sys
+import sys, rich
+from rich import print as rprint
 print("Welcome!\nThis script will convert binary to decimal!")
 while True:
     binary = input("Enter your binary string.\n")
@@ -9,13 +10,13 @@ while True:
     #converts input to decimal. 2 represents the base.
     print("Your decimal is {}".format(decimal))
     while True:
+        rprint("[yellow]If this is running through the launcher, this doesn't work. I am currently looking for a fix.")
         convertmore = input("Would you like to convert another binary string? (Y/N)\n")
         if convertmore.upper() == "Y":
             break
         elif convertmore.upper() == "N":
            print("Goodbye.")
-           break
+           quit()
            #ends program
         else:
            print("Enter Y or N.\n")
-    break
